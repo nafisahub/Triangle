397f29726900dddc356279741bcac7a09676fe79
nafisahub <77121106+nafisahub@users.noreply.github.com>
2021-03-11 23:47:52 +0000

    Add files via upload
    Red|Blue
    Turtle Triangle

---
+++ name.py
@@ -0,0 +1,22 @@
+import turtle
+
+def triangle(side_length, colour):
+    angle = 120
+    turtle.color(colour, colour)
+    turtle.begin_fill()
+    for side in range(3):
+        turtle.forward(side_length)
+        turtle.right(angle)
+
+    turtle.end_fill()
+
+triangle(400, 'red')
+triangle(300, 'pink')
+triangle(200, 'blue')
+triangle(100, 'yellow')
+
+
+
+
+
+
---
+++ triangle.py
@@ -0,0 +1,15 @@
+import turtle
+
+side_length = 100
+angle = 120
+
+turtle.color('blue', 'blue')
+turtle.begin_fill()
+
+turtle.forward(side_length)
+turtle.right(angle)
+turtle.forward(side_length)
+turtle.right(angle)
+turtle.forward(side_length)
+turtle.right(angle)
+turtle.done()
